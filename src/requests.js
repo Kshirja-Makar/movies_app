2bb703d4e1cb7ef9e17f0b15bb13e7cd69114a6f
rpjk <kshirjamakar121@gmail.com>
2021-07-22 20:01:37 +0530

    commit

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const APIKEY = "12d26cbc90b543705fb1fcc7c295b81d";
+
+
+const requests = {
+    fetchTreding: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+    fetchNetFlixOriginals:`/discover/tv?api_key=${APIKEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${APIKEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${APIKEY}&with_genres=35`,
+    fetchHorroMovies:`/discover/movie?api_key=${APIKEY}&with_genres=27`,
+    fetchRomanticMovies:`/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${APIKEY}&with_genres=99`
+}
+
+export default requests;
